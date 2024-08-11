8680933f75d29b4a3a4fdb105104d49741d18cd1
Danny Hidalgo <dnnysongs@gmail.com>
2024-08-11 11:42:25 -0500

    deploy fix

--- pittyproject/settings.py
+++ pittyproject/settings.py
@@ -45,7 +45,7 @@ CORS_ORIGIN_WHITELIST = (
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://pitty-party-client.vercel.app/",
+    "https://pitty-party-client.vercel.app",
 )
 
 
