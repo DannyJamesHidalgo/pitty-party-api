c04e5f4ceeae3484c86b74efa4c7d93780d09e76
Danny Hidalgo <dnnysongs@gmail.com>
2024-08-20 14:05:21 -0500

    fixture missing from bash script re added

--- seed_database.sh
+++ seed_database.sh
@@ -7,7 +7,7 @@ python3 manage.py makemigrations pittyapi
 python3 manage.py migrate pittyapi
 python3 manage.py loaddata users
 python3 manage.py loaddata tokens
-# python3 manage.py loaddata adopter_fixtures
+python3 manage.py loaddata adopter_fixtures
 python3 manage.py loaddata dog_fixtures
 python3 manage.py loaddata events
 
